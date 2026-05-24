19cf97c0e29eb443765f26c5ee1a26d52059e339
Itaneyy <itaneyaluno2003@gmail.com>
2026-05-24 13:30:04 -0300

    atividades bootstrap

--- 4.BancoDados/routes/rotasIndex.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controllerIndex = require('../controller/controllerIndex.js')
-/* GET home page. */
-router.get('/', controllerIndex.tela_principal);
-/* GET página sobre */
-router.get('/sobre', controllerIndex.sobre);
-module.exports = router;
